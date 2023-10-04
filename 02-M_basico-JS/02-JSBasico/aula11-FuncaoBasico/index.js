038c20d9ba26e10f16d0f2f6d45618e57a378270
Clairtonlm <clairtonlm@gmail.com>
2023-10-03 22:59:59 -0300

    Basico de função

--- 02-M_basico-JS/02-JSBasico/aula11-FuncaoBasico/index.js
+++ 02-M_basico-JS/02-JSBasico/aula11-FuncaoBasico/index.js
@@ -1,4 +1,24 @@
 //Funções Basico
-function saudacao() {
-    
+function saudacao(nome) {
+    return ` Bom dia ${nome}`;
 }
+const variavel = saudacao('clairton');//estar sendo executada-estou chamando a função
+console.log(variavel);
+
+function soma(x=0, y=0){
+    const resultado = x + y;
+    return resultado;
+}
+const resul = soma(5,3)
+console.log(resul);
+
+//função anonima- uma funcao dentro de uma variavel
+const raiz = function(n){
+    return n**0.5
+};
+console.log(raiz(9));
+
+//ArrowFunction
+const raizQ = n=>n**0.5
+    
+console.log(raiz(25));
