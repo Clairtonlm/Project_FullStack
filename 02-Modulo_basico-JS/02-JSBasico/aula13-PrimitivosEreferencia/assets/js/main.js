c9263f5e877ca1068a86033de8b1482bbfe30f79
Clairtonlm <clairtonlm@gmail.com>
2023-10-07 21:40:27 -0300

    Referencia e primitivo

--- 02-Modulo_basico-JS/02-JSBasico/aula13-PrimitivosEreferencia/assets/js/main.js
+++ 02-Modulo_basico-JS/02-JSBasico/aula13-PrimitivosEreferencia/assets/js/main.js
@@ -1,6 +1,15 @@
 //valores primitivos e referencias
-/*Primitivos- string, number, boolean, undefined, null, (bigint, symbol) sao imutaveis
+/*Primitivos- string, number, boolean, undefined, null, (bigint, symbol) sao imutaveis..sao copiados se alterar uma variavel e copiar para outra e se altetar um nao altera a outra
 * as string nao indexadas...0-1-2-3...
-*
-* 
+*Referencia mutavel--> array, object, function---> passados por referencia
+* spreed
+* let b = [...a] faz uma copia e alterando um nao altera a outra
 */
+const a = {
+    nome: "javaScript",
+    sobrenome: 'lima'
+}
+
+const b = {...a};//b fica imdependente é uma copia
+a.nome = 'joao';
+console.log(b);
