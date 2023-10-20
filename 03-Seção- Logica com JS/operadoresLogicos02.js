772697fc0642aea181deea11e157a312e35bd1f9
Clairtonlm <clairtonlm@gmail.com>
2023-10-19 21:55:05 -0300

    OPERADORES LOGICOS

---
+++ 03-Seção- Logica com JS/operadoresLogicos02.js
@@ -0,0 +1,10 @@
+/**
+ * OPERADORES LOGICOS
+ * && -> AND -> E---as duas expresões precisam ser verdadeira para ser verdade
+ * || -> OR -> OU---pelo menos uma tem que ser verdade que satisfaz
+ * ! -> NOT -> NÃO
+ */
+
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
