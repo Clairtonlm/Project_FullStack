8d8a56800d7849697e0a87f0ff2faf6a89239111
Clairtonlm <clairtonlm@gmail.com>
2023-10-19 21:31:53 -0300

    Operadores de cmparação

---
+++ 03-Seção- Logica com JS/operadores.js
@@ -0,0 +1,16 @@
+/**
+ * Operadores sao pertgunta que é feita a liguagem(true ou false)
+ * > maior
+ * >= maior que igual a 
+ * < menor
+ * <= menor que e igaul a 
+ * == igualdade checa valor(nao é recomendado uso pq a JS faz uma conversão por traz)
+ * === igualdade estrita (valor e tipo)
+ * != diferente---nao recomendado uso---
+ * !== diferente estrito (valor e tipo)
+ */
+
+console.log(10 > 5);
+console.log(10 >= 5);
+console.log(10 <= 5);
+console.log(10 !== "10");
