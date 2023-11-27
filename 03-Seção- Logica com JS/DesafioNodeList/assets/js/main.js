3b65c676fb7eff5147deddffefdc5bc742cff8e4
Clairtonlm <clairtonlm@gmail.com>
2023-11-27 20:42:19 -0300

    Corrigindo Bug do P

--- 03-Seção- Logica com JS/DesafioNodeList/assets/js/main.js
+++ 03-Seção- Logica com JS/DesafioNodeList/assets/js/main.js
@@ -3,9 +3,9 @@ const ps = document.querySelectorAll('p')
 //para pegar os estilos que foi computado pelo navegador do html
 const estilo = getComputedStyle(document.body);
 const mudarCorP = estilo.backgroundColor;
-console.log(backgraundColorBody);
+console.log(mudarCorP);
 
 for(let p of ps){
     p.style.backgroundColor = mudarCorP
-    p.style.color = '#FFFFFF'
+    p.style.color = '#fffFFF'
 }
